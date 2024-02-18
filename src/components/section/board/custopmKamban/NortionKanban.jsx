861ec0df740d47c1d82fb81e9342f362b01b77fc
Kadunapikin <ibakoshi27@gmail.com>
2024-02-19 00:02:22 +0100

    create Card component and map through filteredCards to instanciate Card tittles

--- src/components/section/board/custopmKamban/NortionKanban.jsx
+++ src/components/section/board/custopmKamban/NortionKanban.jsx
@@ -9,7 +9,7 @@ export const NortionKanban = () => {
 }
 
 const Board = () => {
-    const [cards, setCards] = useState([]);
+    const [cards, setCards] = useState(DEFAULT_CARDS);
     return (
         <div className='flex h-screen w-full gap-3 overflow-scroll p-12'>
             <Column 
@@ -44,6 +44,42 @@ const Board = () => {
         </div>
     )
 }
+
+
+const Column = ({ title, headingColor, column, cards, setCards }) => {
+    const [active, setActive] = useState(false);
+    const filteredCards = cards.filter((c) => c.column === column);
+    return (
+        <div className='w-56 shrink-0'>
+            <div className='mb-3 flex items-center justify-between'>
+                <h3 className={`font-medium ${headingColor}`}>
+                    {title}
+                </h3>
+                <span className='rounded text-sm text-neutral-400'>
+                    {filteredCards.length}
+                </span>
+            </div>
+            <div className={`h-full w-full transition-colors ${!active ? "bg-neutral-800/50" : "bg-neutral-800/0"
+            }`}
+            >
+                {filteredCards.map((c) => {
+                    return <Card key={c.id} {...c}/>;
+                })}
+            </div>
+        </div>
+    )
+}
+
+const Card = ({ title, id, column }) => {
+    return (
+        <>
+            <div>
+                <p className='text-sm text-neutral-100'>{title}</p>
+            </div>
+        </>
+    )
+}
+
 
 const DEFAULT_CARDS = [
     //Backlog
@@ -101,23 +137,3 @@ const DEFAULT_CARDS = [
         column: 'done'
     }
 ]
-
-
-const Column = ({ title, headingColor, column, cards, setCards }) => {
-    const [active, setActive] = useState(false);
-    return (
-        <div className='w-56 shrink-0'>
-            <div className='mb-3 flex items-center justify-between'>
-                <h3 className={`font-medium ${headingColor}`}>
-                    {title}
-                </h3>
-                <span className='rounded text-sm text-neutral-400'>
-                    {cards.length}
-                </span>
-            </div>
-            <div className={`h-full w-full transition-colors ${!active ? "bg-neutral-800/50" : "bg-neutral-800/0"
-            }`}
-            ></div>
-        </div>
-    )
-}
