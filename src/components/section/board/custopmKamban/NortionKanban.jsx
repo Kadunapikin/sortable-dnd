0f09351c5c93e8e6b13ffb3aed6d64b8d59864cc
Kadunapikin <ibakoshi27@gmail.com>
2024-02-18 12:40:14 +0100

    add column instance in Board with different title

--- src/components/section/board/custopmKamban/NortionKanban.jsx
+++ src/components/section/board/custopmKamban/NortionKanban.jsx
@@ -12,7 +12,35 @@ const Board = () => {
     const [cards, setCards] = useState([]);
     return (
         <div className='flex h-screen w-full gap-3 overflow-scroll p-12'>
-            Board
+            <Column 
+                title='Backlog'
+                column='backlog'
+                headingColor='text-neautral-500'
+                cards={cards}
+                setCards={setCards}
+            />
+            <Column 
+                title='TODO'
+                column='todo'
+                headingColor='text-yellow-200'
+                cards={cards}
+                setCards={setCards}
+            />
+            <Column 
+                title='In Progress'
+                column='doing'
+                headingColor='text-blue-200'
+                cards={cards}
+                setCards={setCards}
+            />
+            <Column 
+                title='Complete'
+                column='done'
+                headingColor='text-emerald-200'
+                cards={cards}
+                setCards={setCards}
+            />
+
         </div>
     )
             
@@ -21,6 +49,15 @@ const Board = () => {
 
 const Column = ({ title, headingColor, column, cards, setCards }) => {
     return (
-        <div></div>
+        <div className='w-56 shrink-0'>
+            <div className='mb-3 flex items-center justify-between'>
+                <h3 className={`font-medium ${headingColor}`}>
+                    {title}
+                </h3>
+                <span className='rounded text-sm text-neutral-400'>
+                    {cards.length}
+                </span>
+            </div>
+        </div>
     )
 }
