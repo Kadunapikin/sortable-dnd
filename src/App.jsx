5e131d2720467b9e8ec11d4a9673db15cc47c973
Kadunapikin <ibakoshi27@gmail.com>
2024-02-18 11:51:16 +0100

    create Board component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { NortionKanban } from './components/section/board/custopmKamban/NortionKanban'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-black'>
-        Hello world! 
+      <div className=''>
+        <NortionKanban />
       </div>
     </>
   )
--- src/components/section/board/custopmKamban/NortionKanban.jsx
+++ src/components/section/board/custopmKamban/NortionKanban.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export const NortionKanban = () => {
   return (
-    <div>
-        NortionKanban
+    <div className='h-screen w-full bg-neutral-900 text-neutral-50'>
+        <Board />
     </div>
   )
+}
+
+const Board = () => {
+    return (
+        <div className='flex h-screen w-full gap-3 overflow-scroll p-12'>
+            Board
+        </div>
+    )
+            
 }
