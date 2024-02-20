172f1f71ef896ca6b054cfc7157e19a61197535d
Kadunapikin <ibakoshi27@gmail.com>
2024-02-20 23:06:48 +0100

    add draggable html property

--- src/components/section/board/custopmKamban/NortionKanban.jsx
+++ src/components/section/board/custopmKamban/NortionKanban.jsx
@@ -73,7 +73,10 @@ const Column = ({ title, headingColor, column, cards, setCards }) => {
 const Card = ({ title, id, column }) => {
     return (
         <>
-            <div className='cursor-grab rounded before border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing'>
+            <div
+            draggable='true' 
+            className='cursor-grab rounded before border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing'
+            >
                 <p className='text-sm text-neutral-100'>{title}</p>
             </div>
         </>
