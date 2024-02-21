6d45b5af0e136d79b84e9ddc13f660e4ec3ee8c1
Kadunapikin <ibakoshi27@gmail.com>
2024-02-21 23:34:32 +0100

    add textarea and buttons to AddCart component

--- src/components/section/board/custopmKamban/NortionKanban.jsx
+++ src/components/section/board/custopmKamban/NortionKanban.jsx
@@ -115,11 +115,28 @@ const AddCart = ({ column, setCards }) => {
     const [text, setText] = useState("");
     const [adding, setAdding] = useState(false);
     return <>
-        {adding ? <></> : <button onClick={() => setAdding(true)} className='flex w-full items-center gap-1.5 px-3 py-1.5 text-xs text-neutral-400 transition-colors hover:text-neutral-50'
+        {adding ? (
+            <form>
+                <textarea 
+                onChange={(e) => setText(e.target.value)}
+                autoFocus
+                placeholder='Add New Task...'
+                className='w-full rounded border border-violet-400 bg-violet-400/20 p-3 text-sm text-neutral-50 placeholder-violet-300 focus:outline-0'/>
+                <div onClick={() => setAdding(false)}
+                className='mt-1.5 flex items-center justify-end gap-1.5'
+                >
+                    <button className='px-3 py-1.5 text-sm text-neutral-400 transition-colors hover:text-neutral-50'>
+                        Close
+                    </button>
+                </div>
+            </form>
+        ) : (
+            <button onClick={() => setAdding(true)} className='flex w-full items-center gap-1.5 px-3 py-1.5 text-xs text-neutral-400 transition-colors hover:text-neutral-50'
             >
                 <span>Add Card</span>
                 <FiPlus />
-            </button>}
+            </button>)
+        }
         </>
 }
 
