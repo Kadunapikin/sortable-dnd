41782da5d0e73cebd1c83c0fa5da18e8862dced8
Kadunapikin <ibakoshi27@gmail.com>
2024-02-21 18:46:52 +0100

    add react icons to BurnBarell component

--- src/components/section/board/custopmKamban/NortionKanban.jsx
+++ src/components/section/board/custopmKamban/NortionKanban.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { FaFire } from 'react-icons/fa';
+import { FiTrash } from 'react-icons/fi';
 
 export const NortionKanban = () => {
   return (
@@ -96,14 +98,14 @@ const DropIndicator = ({ beforeId, column }) => {
 }
 
 const BurnBarell = ({ setCards }) => {
-    const [active, setActive] = useState(true);
+    const [active, setActive] = useState(false);
     return (
         <div className={`mt-10 grid h-56 w-56 shrink-0 place-content-center rounded border text-3xl ${
             active 
             ? "border-red-800 bg-red-800/20 text-red-500" 
             : "border-neutral-500 bg-neutral-500/20 text-neutral-500"
             }`}
-            ></div>
+            >{active ? <FaFire className="animate-bounce" /> : <FiTrash />}</div>
     )
 }
 
